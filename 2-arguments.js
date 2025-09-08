acb3ae81c1b3e64cb10ea4b6721d3a755668fa73
mstf-ai <albrnsmstfy252@gmail.com>
2025-09-08 19:14:42 +0300

    M

---
+++ 2-arguments.js
@@ -0,0 +1,22 @@
+2. Arguments
+mandatory
+Write a script that prints a message depending of the number of arguments passed:
+
+If no arguments are passed to the script, print “No argument”
+If only one argument is passed to the script, print “Argument found”
+Otherwise, print “Arguments found”
+You must use console.log(...) to print all output
+You are not allowed to use var
+Reference: process.argv
+
+guillaume@ubuntu:~/0x12$ node 2-arguments.js 
+No argument
+guillaume@ubuntu:~/0x12$ node 2-arguments.js Best
+Argument found
+guillaume@ubuntu:~/0x12$ node 2-arguments.js Best School
+Arguments found
+guillaume@ubuntu:~/0x12$ 
+Repo:
+
+GitHub repository: JS-Fundamentals
+File: 2-arguments.js
