9c45ba3a635488da29cff9f3b96b3cc1359becb4
mstf-ai <albrnsmstfy252@gmail.com>
2025-09-08 19:16:37 +0300

    M

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,22 +1,12 @@
-2. Arguments
-mandatory
-Write a script that prints a message depending of the number of arguments passed:
+#!/usr/bin/node
 
-If no arguments are passed to the script, print “No argument”
-If only one argument is passed to the script, print “Argument found”
-Otherwise, print “Arguments found”
-You must use console.log(...) to print all output
-You are not allowed to use var
-Reference: process.argv
+const argsCount = process.argv.length - 2;
 
-guillaume@ubuntu:~/0x12$ node 2-arguments.js 
-No argument
-guillaume@ubuntu:~/0x12$ node 2-arguments.js Best
-Argument found
-guillaume@ubuntu:~/0x12$ node 2-arguments.js Best School
-Arguments found
-guillaume@ubuntu:~/0x12$ 
-Repo:
+if (argsCount === 0) {
+	  console.log("No argument");
+} else if (argsCount === 1) {
+	  console.log("Argument found");
+} else {
+	  console.log("Arguments found");
+}
 
-GitHub repository: JS-Fundamentals
-File: 2-arguments.js
